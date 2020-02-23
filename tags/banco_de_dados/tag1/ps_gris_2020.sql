bfb3e050b24a52e00fbba6a872b9e3b4668506ce
FJAConceicao <lipeconceicao13@gmail.com>
2020-02-22 23:56:34 -0300

    Atualização/Desenvolvimento da tag 1 de banco de dados

--- tags/banco_de_dados/tag1/ps_gris_2020.sql
+++ tags/banco_de_dados/tag1/ps_gris_2020.sql
@@ -2,33 +2,41 @@
 -- Processo Seletivo GRIS 2020
 -- Avaliador: Arthur Castro
 -- TAG: Criar banco de dados do Processo seletivo do GRIS 2020
+-- Candidato: Felipe de Jesus
 ------------------------------------------------------------------
 
 -- criando banco de dados
-CREATE DATABASE psGRIS2020;
+CREATE DATABASE PSGRIS2020;
 
 -- ativando banco de dados
-USE psGRIS2020;
+USE PSGRIS2020;
 
 -- criando tabelas do banco dados do psGRIS2020
-CREATE TABLE candidato (
+CREATE TABLE Candidato (
+	candidatoID int NOT NULL,
 	nome_completo varchar(255),
 	dre varchar(9),
 	email varchar(255).
-	data_de_nascimento date,
 	nome_de_usuario_telegram varchar(32),
+	PRIMARY KEY (CandidatoID)
 );
 
-CREATE TABLE tags (
+CREATE TABLE Tags (
+	tagID int NOT NULL,
 	nome_da_tag varchar(100),
 	assunto varchar(1000),
 	objetivo varchar(500),
 	dicas varchar(500),
 	data_de_entrega date,
+	PRIMARY KEY (tagID)
 );
 
-CREATE TABLE avaliacao (
-	nome_do_candidato varchar(255),
-	tags_feitas,
-	nota int(2),
+CREATE TABLE Avaliacao (
+	avaliacaoID NOT NULL,
+	candidatoID int NOT NULL,
+	tagID int NOT NULL,
+	nota int(2) NOT NULL,
+	PRIMARY KEY (avaliacaoID),
+	FOREIGN KEY (candidatoID) REFERENCES Candidato(candidatoID),
+	FOREIGN KEY (tagID) REFERENCES Tags(tagID)
 );
