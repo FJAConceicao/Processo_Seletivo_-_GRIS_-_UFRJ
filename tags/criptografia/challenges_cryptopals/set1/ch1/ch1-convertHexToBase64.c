2359b2b8725eea2f82763fb3f06a94a77750cf5f
FJAConceicao <lipeconceicao13@gmail.com>
2020-02-25 01:23:32 -0300

    Organização e simplificação dos desafios 1 e 2 da tag de Criptografia

--- tags/criptografia/challenges_cryptopals/set1/ch1/ch1-convertHexToBase64.c
+++ tags/criptografia/challenges_cryptopals/set1/ch1/ch1-convertHexToBase64.c
@@ -1,101 +1,101 @@
-//Programa que converte números em Hexadecimal para base64
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-/*define tabelaBase64[64] "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"*/
+
+#define BASE64TABLE "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+#define BASE16TABLE "0123456789abcdefABCDEF"
 
 int main()
-{	
-	const char tabelaBase64[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+{
+    /**Criando variaveis que vão ser utilizadas ao longo do programa**/
+    int index, count = 0, var = 0;
 
-	//criando string com tamanho maximo de 255 caracteres
-	char string_hex[255];
-	printf("Informe uma string em hexadecimal: ");
-	scanf("%s", string_hex);
+    char stringBinary[1000] = "", stringInBase64[1000] = "";
+    
+    //array com numeros de 1 ate 15 da base hexadecimal para a base binaria com 4 bits
+	const char hexadecimalToBinary[16][5] = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
+    
+	/**criando string hexadecimal que irá guardar entrada do usuário à ser pedida logo na 2ª linha abaixo**/
+	char stringHexadecimal[255];
+    printf("Informe uma string em base hexadecimal:\n\n");
+	scanf(" %[^\n]s", stringHexadecimal);
+    
+    //removendo espaços da string em hexadecimal, caso tenha
+    for(index = 0; stringHexadecimal[index] != '\0'; ++index) {
+        if(stringHexadecimal[index] != ' ') {
+            stringHexadecimal[count] = stringHexadecimal[index];
+            ++count;
+        }
+    }
+    stringHexadecimal[count] = '\0';
+    
+    /*
+    *parte que verifica se string informada pelo usuário esta correta
+    *se não estiver correta, mostra uma mensagem de erro e pede a string novamente
+    */
+    while(var == 0) {
+        var = 1;
+        for(index = 0; stringHexadecimal[index] != '\0'; ++index) {
+            if(strchr(BASE16TABLE, stringHexadecimal[index]) == NULL) {
+                var = 0;
+                printf("\n[BASE_INCORRETA!] Informe a string corretamente ...\n\n");
+                scanf(" %[^\n]s", stringHexadecimal);
+                break;
+            }
+        }
+    }
 	
-	//TODO: Fazer validação da string em hexadecimal
-	
-	//array com numeros de 1 ate 15 da base hexadecimal para a base binaria com 4 bits
-	char hex_to_bin[16][5] = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
-	
-	//variavel para armazenar a conversao da string hexadecimal para binario
-	//int len_string_hex = (strlen(string_hex) * 4) + 20;
-	char string_bin[1000] = "";
-	
-	//convertendo a string em hexadecimal e armazenando na string em binario
-	int i;
-	for(i = 0; string_hex[i] != '\0'; ++i)
-	{
-		if((string_hex[i] - '0') >= 0 && (string_hex[i] - '0') <= 9)
-		{
-			strcat(string_bin, hex_to_bin[string_hex[i] - '0']);
-		}
-		else
-		{
-			if(string_hex[i] == 'a' || string_hex[i] == 'A')
-				strcat(string_bin, hex_to_bin[10]);
-			else if(string_hex[i] == 'b' || string_hex[i] == 'B')
-				strcat(string_bin, hex_to_bin[11]);
-			else if(string_hex[i] == 'c' || string_hex[i] == 'C')
-                                strcat(string_bin, hex_to_bin[12]);
-			else if(string_hex[i] == 'd' || string_hex[i] == 'D')
-                                strcat(string_bin, hex_to_bin[13]);
-			else if(string_hex[i] == 'e' || string_hex[i] == 'E')
-                                strcat(string_bin, hex_to_bin[14]);
-			else if(string_hex[i] == 'f' || string_hex[i] == 'F')
-                                strcat(string_bin, hex_to_bin[15]);
+	/*convertendo a string em hexadecimal para binário*/
+	for(index = 0; stringHexadecimal[index] != '\0'; ++index) {
+		if((stringHexadecimal[index] - '0') >= 0 && (stringHexadecimal[index] - '0') <= 9) {
+			strcat(stringBinary, hexadecimalToBinary[stringHexadecimal[index] - '0']);
+		}else{
+			if(stringHexadecimal[index] == 'a' || stringHexadecimal[index] == 'A')
+				strcat(stringBinary, hexadecimalToBinary[10]);
+			else if(stringHexadecimal[index] == 'b' || stringHexadecimal[index] == 'B')
+				strcat(stringBinary, hexadecimalToBinary[11]);
+			else if(stringHexadecimal[index] == 'c' || stringHexadecimal[index] == 'C')
+                                strcat(stringBinary, hexadecimalToBinary[12]);
+			else if(stringHexadecimal[index] == 'd' || stringHexadecimal[index] == 'D')
+                                strcat(stringBinary, hexadecimalToBinary[13]);
+			else if(stringHexadecimal[index] == 'e' || stringHexadecimal[index] == 'E')
+                                strcat(stringBinary, hexadecimalToBinary[14]);
+			else if(stringHexadecimal[index] == 'f' || stringHexadecimal[index] == 'F')
+                                strcat(stringBinary, hexadecimalToBinary[15]);
 		}
 	}
-
-	//Essa parte acrescenta zeros na string binária até que ela seja divisivel por 6
-	int cont = 0;
-	while(strlen(string_bin)%6 != 0)
-	{
-		strcat(string_bin, "0");
-		++cont;
+    
+    count = 0;
+    
+	/*Essa parte acrescenta zeros na string binária até que ela seja divisivel por 6*/
+	while(strlen(stringBinary)%6 != 0){
+		strcat(stringBinary, "0");
+		++count;
 	}
-
-	/*
-	printf("%ld\n", strlen(string_bin));	
-	printf("String binária: %s\n", string_bin);
-	*/
-	
-	int num;
-	char stringConvertBase64[1000] = "";
-
-	for(i = 0; i < strlen(string_bin); i += 6)
-	{
-		//Definindo uma substring com 6 bits da string_bin para converter para decimal e obter o valor na tabela de base64
-		num = i;
-		char byteInBase64[7] = "";
-		while(num < (i + 6))
-		{
-			strncat(byteInBase64, &string_bin[num], 1);
-			++num;
-		}
-		
-		printf("Byte in Base 64: %s\n", byteInBase64);
+    
+    /*Parte que na stringBinary converte cada 6 bits para decimal da esquerda para a direita e concatena seu valor em base64 com stringInBase64*/
+	for(index = 0; stringBinary[index] != '\0'; index += 6) {
+        
+        //definindo uma substring com 6 bits da stringBinary para converter para decimal e obter o valor na tabela de base64
+		char byteInBase64[6] = "";
+        for(var = index; var < (index + 6); ++var)
+            strncat(byteInBase64, &stringBinary[var], 1);
 		
 		//converter byteInBase64 para decimal
-		int j, numberOfCharInBase64 = 0;
-		for(j = 0, num = 5; num >= 0; --num, ++j)
-		{
-			numberOfCharInBase64 += (pow(2, num) * (byteInBase64[j] - '0'));
+		int numberOfCharInBase64 = 0;
+		for(var = 5; var >= 0; --var) {
+			numberOfCharInBase64 += (pow(2, var) * (byteInBase64[5 - var] - '0'));
 		}
 
-		printf("Number Of Char In Base 64: %d\n\n", numberOfCharInBase64);
-
 		//concatenar numero correspondente ao numberOfCharInBase64 na tabela de base 64 com a variável que armazena a conversão para base 64
-		strncat(stringConvertBase64, &tabelaBase64[numberOfCharInBase64], 1);
+		strncat(stringInBase64, &BASE64TABLE[numberOfCharInBase64], 1);
 	}
 	
 	//colocando padding no final da conversão, caso seja necessário
 	char padding = '=';
-	strncat(stringConvertBase64, &padding, (int)(cont/2));
+	strncat(stringInBase64, &padding, (int)(count/2));
 
-	printf("String hexadecimal: %s\n", string_hex);
-	printf("String binária....: %s\n", string_bin);
-	printf("String em base64..: %s\n", stringConvertBase64);
+	printf("\nResultado da conversão para base 64: \n\n%s\n", stringInBase64);
 
 	return 0;
 }
--- tags/criptografia/challenges_cryptopals/set1/ch2/ch2_fixed_xor.c
+++ tags/criptografia/challenges_cryptopals/set1/ch2/ch2_fixed_xor.c
@@ -1,103 +1,75 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
 int main()
 {
-	char strings[2][1000];
-	char stringsInBinario[2][1000] = {};
-	char binaryFinal[1000] = "";
-	char resultXor[1000] = "";
+    int column, line, binaryFinalToDecimal = 0;
+	char caracter, strings[2][1000], stringsInBinario[2][1000] = {}, binaryFinal[1000] = "", resultXor[1000] = "";
 
+    //array com numeros de 1 ate 15 da base hexadecimal para a base binaria com 4 bits
+    const char hexToBin[16][5] = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
+    
 	printf("Informe valores hexadecimais de mesmo tamanho para as strings abaixo: \n\n");
-
-	printf("String 1: "); scanf("%s", strings[0]);
-	printf("String 2: "); scanf("%s", strings[1]);
-	
-	//array com numeros de 1 ate 15 da base hexadecimal para a base binaria com 4 bits
-        char hexToBin[16][5] = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
+	printf("String 1 : "); scanf(" %[^\n]s", strings[0]);
+	printf("String 2 : "); scanf(" %[^\n]s", strings[1]);
 	
 	//CONVERTER AMBAS STRINGS PARA BINÁRIO E ARMAZENAR EM UM ARRAY COM 2 POSIÇÕES: 1 PARA CADA STRING
-	int i, j, c;
-	for(i = 0; strings[0][i] != '\0'; ++i)
-	{
-		for(j = 0; j <= 1; ++j)
-		{
-			if((strings[j][i] - '0') >= 0 && (strings[j][i] - '0') <= 9)
-			{
-				strcat(stringsInBinario[j], hexToBin[(strings[j][i] - '0')]);
-			}
-			else
-			{
-				if(strings[j][i] == 'a' || strings[j][i] == 'A')
-					strcat(stringsInBinario[j], hexToBin[10]);
-				else if(strings[j][i] == 'b' || strings[j][i] == 'B')
-                                        strcat(stringsInBinario[j], hexToBin[11]);
-				else if(strings[j][i] == 'c' || strings[j][i] == 'C')
-                                        strcat(stringsInBinario[j], hexToBin[12]);
-				else if(strings[j][i] == 'd' || strings[j][i] == 'D')
-                                        strcat(stringsInBinario[j], hexToBin[13]);
-				else if(strings[j][i] == 'e' || strings[j][i] == 'E')
-                                        strcat(stringsInBinario[j], hexToBin[14]);
-				else if(strings[j][i] == 'f' || strings[j][i] == 'F')
-                                        strcat(stringsInBinario[j], hexToBin[15]);
+	for(column = 0; strings[0][column] != '\0'; ++column) {
+		for(line = 0; line <= 1; ++line) {
+			if((strings[line][column] - '0') >= 0 && (strings[line][column] - '0') <= 9) {
+				strcat(stringsInBinario[line], hexToBin[(strings[line][column] - '0')]);
+			} else {
+				if(strings[line][column] == 'a' || strings[line][column] == 'A')
+					strcat(stringsInBinario[line], hexToBin[10]);
+				else if(strings[line][column] == 'b' || strings[line][column] == 'B')
+                    strcat(stringsInBinario[line], hexToBin[11]);
+				else if(strings[line][column] == 'c' || strings[line][column] == 'C')
+                    strcat(stringsInBinario[line], hexToBin[12]);
+				else if(strings[line][column] == 'd' || strings[line][column] == 'D')
+                    strcat(stringsInBinario[line], hexToBin[13]);
+				else if(strings[line][column] == 'e' || strings[line][column] == 'E')
+                    strcat(stringsInBinario[line], hexToBin[14]);
+				else if(strings[line][column] == 'f' || strings[line][column] == 'F')
+                    strcat(stringsInBinario[line], hexToBin[15]);
 			}
 		}
 	}
 
 	//FAZER OPERAÇÃO XOR ENTRE AS DUAS STRINGS BIT A BIT
-	char caracter;
-	for(i = 0; stringsInBinario[0][i] != '\0'; ++i)
-	{
-		caracter = ((stringsInBinario[0][i] - '0')^(stringsInBinario[1][i] - '0')) + '0';
+	for(column = 0; stringsInBinario[0][column] != '\0'; ++column) {
+		caracter = ((stringsInBinario[0][column] - '0') ^ (stringsInBinario[1][column] - '0')) + '0';
 		strncat(binaryFinal, &caracter, 1);
 	}
- 	
+    
 	//CONVERTER binaryFinal PARA BASE 16
-	int number = 0;
-	char xor;
-	for(i = 0; binaryFinal[i] != '\0'; i += 4)
-	{
-		for(c=i, j=3; j > -1; --j, ++c)
-		{
-			number += (pow(2, j) * (binaryFinal[c] - '0'));
+	for(column = 0; binaryFinal[column] != '\0'; ) {
+		for(line = 3; line >= 0 ; --line) {
+			binaryFinalToDecimal += (pow(2, line) * (binaryFinal[column++] - '0'));
 		}
 	
-		if(number >= 0 && number <= 9)
-		{
-			xor = (number + '0');
-			strncat(resultXor, &xor, 1);
-		}
-		else
-		{
-			if(number == 10){
-				xor = 'a';
-			       	strncat(resultXor, &xor, 1);
-			}else if(number == 11){
-				xor = 'b';
-				strncat(resultXor, &xor, 1);
-			}else if(number == 12){
-                                xor = 'c';
-                                strncat(resultXor, &xor, 1);
-			}else if(number == 13){
-                                xor = 'd';
-                                strncat(resultXor, &xor, 1);
-			}else if(number == 14){
-                                xor = 'e';
-                                strncat(resultXor, &xor, 1);
-			}else if(number == 15){
-                                xor = 'f';
-                                strncat(resultXor, &xor, 1);
+		if(binaryFinalToDecimal >= 0 && binaryFinalToDecimal <= 9) {
+			caracter = (binaryFinalToDecimal + '0');
+			strncat(resultXor, &caracter, 1);
+		} else {
+			if(binaryFinalToDecimal == 10){
+                strcat(resultXor, "a");
+			} else if(binaryFinalToDecimal == 11){
+				strcat(resultXor, "b");
+			} else if(binaryFinalToDecimal == 12){
+                strcat(resultXor, "c");
+			} else if(binaryFinalToDecimal == 13){
+                strcat(resultXor, "d");
+			} else if(binaryFinalToDecimal == 14){
+                strcat(resultXor, "e");
+			} else if(binaryFinalToDecimal == 15){
+                strcat(resultXor, "f");
 			}
 		}
-		number = 0;
+		binaryFinalToDecimal = 0;
 	}
 	
-	printf("String 1 - Binaria: %s\n", stringsInBinario[0]);
-	printf("String 2 - Binaria: %s\n", stringsInBinario[1]);
-	printf("Str1 XOR Str2.....: %s\n", binaryFinal);
-	printf("Resultado Final...: %s\n", resultXor);
+	printf("Fixed XOR: %s\n", resultXor);
 
 	return 0;
 }
